55c29771d08c568d9705213720fee33c147f73fd
danielSafonyan <danielsafonyan@gmail.com>
2023-02-01 14:13:19 +0000

    feat: add router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import './main.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Home</div>,
+  },
+  {
+    path: "/projects",
+    element: <div>Projects</div>,
+  },
+  {
+    path: "/about",
+    element: <div>About & CV</div>,
+  },
+  {
+    path: "/contact",
+    element: <div>About & CV</div>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
