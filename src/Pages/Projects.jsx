6592385ab7be9e82a0a77e14e1a7cb75429a5ba7
danielSafonyan <danielsafonyan@gmail.com>
2023-02-02 14:06:05 +0000

    feat: filter projects by tech

---
+++ src/Pages/Projects.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import Header from '../components/Header'
+import Socials from '../components/Socials'
+import Project from '../components/Project'
+
+import { NavLink } from "react-router-dom";
+
+import projectData from '../assets/projectData'
+
+export default function Home() {
+    const [currentFilter, setCurrentFilter] = React.useState('All')
+    // const projectElems = projectData.map((el, idx) => <Project key={idx} {...el} />)
+    let projectElems
+    
+    if (currentFilter === 'All') {
+        projectElems = projectData.map((el, idx) => <Project key={idx} {...el} />)
+    } else {
+        projectElems = projectData.filter(el => el.technology === currentFilter).map((el, idx) => <Project key={idx} {...el} />)
+    }
+
+    return (
+        <section className="home-hero">
+            <div className="home-hero__content">
+                <div className="home-hero__info">
+                <p className="text-primary"> 
+                    This section is a showcase of my web development skills and experience. 
+                </p>
+                <p className="text-primary"> 
+                    Projects are built using Vanilla JavaScript or the React framework with React Hooks and React Router and are accompanied by a live demonstration and a source code.
+                </p>
+                </div>
+            </div>
+            <div className="filters">
+                    <div 
+                        className={`filter-btn ${currentFilter === 'All' ? 'active-btn' : ''}`}
+                        onClick={() => setCurrentFilter('All')}
+
+                    >All</div>
+                    <div 
+                        className={`filter-btn ${currentFilter === 'Vanilla JS' ? 'active-btn' : ''}`}
+                        onClick={() => setCurrentFilter('Vanilla JS')}
+                    >Vanilla JS</div>
+                    <div 
+                        className={`filter-btn ${currentFilter === 'React' ? 'active-btn' : ''}`}
+                        onClick={() => setCurrentFilter('React')}
+                    >React</div>
+                </div>
+            <section className='projects-container'>
+                {projectElems}
+            </section>
+    </section>
+        )
+}
+
+// name: 'Hacker News Clone',
+//         previewImg: 'https://i.ibb.co/1mytnxQ/hackernews-clone.png',
+//         previewLink: 'https://danielsafonyan.github.io/hackernews-clone/',
+//         gitHubLink: 'https://github.com/danielSafonyan/hackernews-clone',
+//         technology: 'Vanilla JS'
