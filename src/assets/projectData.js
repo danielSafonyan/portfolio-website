95c4c2723988122b64e79a8c664230adb2b26f30
danielSafonyan <danielsafonyan@gmail.com>
2023-02-02 13:59:25 +0000

    chore: add projects data file

---
+++ src/assets/projectData.js
@@ -0,0 +1,77 @@
+const projectData = [
+    {
+        name: 'Hacker News Clone',
+        previewImg: 'https://i.ibb.co/2yMnV42/hackernews-clone.jpg',
+        previewLink: 'https://danielsafonyan.github.io/hackernews-clone/',
+        gitHubLink: 'https://github.com/danielSafonyan/hackernews-clone',
+        technology: 'Vanilla JS',
+        description: 'This project is a clone of the popular social news website Hacker News built with Vanilla JavaScript, HTML, and CSS.'
+    },
+    {
+        name: 'Keep Notes',
+        previewImg: 'https://i.ibb.co/sWXvscX/keep.jpg',
+        previewLink: 'https://danielsafonyan.github.io/keep-notes/',
+        gitHubLink: 'https://github.com/danielSafonyan/keep-notes',
+        technology: 'Vanilla JS',
+        description: 'A simple and easy-to-use note-taking application built with Vanilla JavaScript, HTML, and CSS. '
+    },
+    {
+        name: 'Movie Watchlist',
+        previewImg: 'https://i.ibb.co/WBBKzZn/watchlist.jpg',
+        previewLink: 'https://danielsafonyan.github.io/react-watchlist/',
+        gitHubLink: 'https://github.com/danielSafonyan/react-watchlist',
+        technology: 'React',
+        description: 'A simple and user-friendly movie watchlist app built with React, React Router, and localStorage. '
+    },
+    {
+        name: 'Typing Speed',
+        previewImg: 'https://i.ibb.co/TkNvjWb/typing.jpg',
+        previewLink: 'https://danielsafonyan.github.io/react-hooks-typing/',
+        gitHubLink: 'https://github.com/danielSafonyan/react-hooks-typing',
+        technology: 'React',
+        description: 'A speed typing app built with React and React Hooks. It was built to practice React, thus lacks comprehensive logic.'
+    },
+    {
+        name: 'Word For The Day',
+        previewImg: 'https://i.ibb.co/2dN7NXP/word.jpg',
+        previewLink: 'https://danielsafonyan.github.io/word-of-the-day/',
+        gitHubLink: 'https://github.com/danielSafonyan/word-of-the-day',
+        technology: 'Vanilla JS',
+        description: 'An app for learning new words. A user inputs a word, the app outputs and image to set as a phone wallpaper.'
+    },
+    {
+        name: 'Deep Concentration',
+        previewImg: 'https://i.ibb.co/64tjkm3/concentration.jpg',
+        previewLink: 'https://danielsafonyan.github.io/deep-concentration-tracker/',
+        gitHubLink: 'https://github.com/danielSafonyan/deep-concentration-tracker',
+        technology: 'Vanilla JS',
+        description: 'A web application for tracking the time spent on a task with background music through Spotify player embed.'
+    },
+    {
+        name: 'TODO List',
+        previewImg: 'https://i.ibb.co/tbJ28XR/todo.jpg',
+        previewLink: 'https://danielsafonyan.github.io/odin-to-do-list/',
+        gitHubLink: 'https://github.com/danielSafonyan/odin-to-do-list',
+        technology: 'React',
+        description: 'An app built with React and localStorage. Users can add, delete, copy and move notes into a "For Today" folder.'
+    },
+    {
+        name: 'Food Ordering App',
+        previewImg: 'https://i.ibb.co/0J3P2R6/food.jpg',
+        previewLink: 'https://danielsafonyan.github.io/restaurant-mobile-ordering-app/',
+        gitHubLink: 'https://github.com/danielSafonyan/restaurant-mobile-ordering-app',
+        technology: 'Vanilla JS',
+        description: 'A Food Ordering App built with Vanilla JS, HTML and CSS. Food items are imported from JSON like file.'
+    },
+    {
+        name: 'Trivia Quiz',
+        previewImg: 'https://i.ibb.co/RN82LT0/quiz.jpg',
+        previewLink: 'https://danielsafonyan.github.io/react-quizz/',
+        gitHubLink: 'https://github.com/danielSafonyan/react-quizz',
+        technology: 'React',
+        description: "A fun and interactive quiz game built with React and Open Trivia Database API to test user's knowledge."
+    },
+
+]
+
+export default projectData
