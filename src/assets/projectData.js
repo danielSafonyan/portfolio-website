f8ca8083bff28aba803701b6483966116a28086c
danielSafonyan <danielsafonyan@gmail.com>
2023-02-02 14:13:28 +0000

    chore: rearranged list

--- src/assets/projectData.js
+++ src/assets/projectData.js
@@ -24,6 +24,22 @@ const projectData = [
         description: 'A simple and user-friendly movie watchlist app built with React, React Router, and localStorage. '
     },
     {
+        name: 'Trivia Quiz',
+        previewImg: 'https://i.ibb.co/RN82LT0/quiz.jpg',
+        previewLink: 'https://danielsafonyan.github.io/react-quizz/',
+        gitHubLink: 'https://github.com/danielSafonyan/react-quizz',
+        technology: 'React',
+        description: "A fun and interactive quiz game built with React and Open Trivia Database API to test user's knowledge."
+    },
+    {
+        name: 'TODO List',
+        previewImg: 'https://i.ibb.co/tbJ28XR/todo.jpg',
+        previewLink: 'https://danielsafonyan.github.io/odin-to-do-list/',
+        gitHubLink: 'https://github.com/danielSafonyan/odin-to-do-list',
+        technology: 'React',
+        description: 'An app built with React and localStorage. Users can add, delete, copy and move notes into a "For Today" folder.'
+    },
+    {
         name: 'Typing Speed',
         previewImg: 'https://i.ibb.co/TkNvjWb/typing.jpg',
         previewLink: 'https://danielsafonyan.github.io/react-hooks-typing/',
@@ -48,28 +64,12 @@ const projectData = [
         description: 'A web application for tracking the time spent on a task with background music through Spotify player embed.'
     },
     {
-        name: 'TODO List',
-        previewImg: 'https://i.ibb.co/tbJ28XR/todo.jpg',
-        previewLink: 'https://danielsafonyan.github.io/odin-to-do-list/',
-        gitHubLink: 'https://github.com/danielSafonyan/odin-to-do-list',
-        technology: 'React',
-        description: 'An app built with React and localStorage. Users can add, delete, copy and move notes into a "For Today" folder.'
-    },
-    {
         name: 'Food Ordering App',
         previewImg: 'https://i.ibb.co/0J3P2R6/food.jpg',
         previewLink: 'https://danielsafonyan.github.io/restaurant-mobile-ordering-app/',
         gitHubLink: 'https://github.com/danielSafonyan/restaurant-mobile-ordering-app',
         technology: 'Vanilla JS',
         description: 'A Food Ordering App built with Vanilla JS, HTML and CSS. Food items are imported from JSON like file.'
-    },
-    {
-        name: 'Trivia Quiz',
-        previewImg: 'https://i.ibb.co/RN82LT0/quiz.jpg',
-        previewLink: 'https://danielsafonyan.github.io/react-quizz/',
-        gitHubLink: 'https://github.com/danielSafonyan/react-quizz',
-        technology: 'React',
-        description: "A fun and interactive quiz game built with React and Open Trivia Database API to test user's knowledge."
     },
 
 ]
