c5774f65672db78a139451a4bc13a2b500f83b49
danielSafonyan <danielsafonyan@gmail.com>
2023-05-08 16:38:48 +0100

    chore: update CV

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -21,10 +21,13 @@ function downloadCV() {
                 <p className="text-primary">
                     In this section you can preview my CV in the browser or download it.
                 </p>
-                <iframe src="https://drive.google.com/file/d/1Z_MtM6lQ3YhpkMFzW7FaTAcqMJimoON5/preview" 
+                {/* <iframe src="https://drive.google.com/file/d/1Z_MtM6lQ3YhpkMFzW7FaTAcqMJimoON5/preview" 
                 height="400"
                 width="100%"
-                allow="autoplay"></iframe>
+                allow="autoplay"></iframe> */}
+                <iframe src="https://docs.google.com/document/d/e/2PACX-1vQHVrOq6FEtHhtjXhigIrl0QOkhrj3y0MlA4xf2cqAiyrXISsKJXgPVpri_4X7ChgmA4h4uChLjsBAB/pub?embedded=true"
+                height="400"
+                width="100%"></iframe>
                 <a href='#' className="btn btn--bg" onClick={downloadCV}>Download CV</a>
                 </div>
             </div>
